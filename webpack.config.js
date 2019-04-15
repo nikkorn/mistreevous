1776ed1a56300a1f5d1c6ae574d03d28d3735b01
Nikolas Howard <nikolas.howard@LighthouseSystems.com>
2019-04-15 13:48:23 +0100

    Added package.json

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+var path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js'
+  }
+};
