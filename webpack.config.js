2d5e931cd93f234e484870728586ffe49c0d610a
Nikolas Howard <nikolas.howard@LighthouseSystems.com>
2019-04-15 13:57:03 +0100

    Fixed webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,6 @@
-var path = require('path');
-
 module.exports = {
-  mode: 'development',
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js'
+    filename: './dist/index.js'
   }
 };
