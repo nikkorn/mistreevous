81e40f0c8b25673d02b1b12acda7360fa1574bf1
nikkorn <nikhoward1234@hotmail.co.uk>
2019-05-12 22:38:53 +0100

    Added guard scope

---
+++ src/guards/guardScope.js
@@ -0,0 +1,12 @@
+
+export default function GuardScope(condition, parent = null) {
+
+    /**
+     * Gets whether the guard is satisfied.
+     * @param board The board.
+     * @returns Whether the guard is satisfied.
+     */
+    this.createScope = (condition) => {
+        // TODO
+    };
+};
