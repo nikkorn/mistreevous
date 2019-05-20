5a0b49386aa40e3cc48161f726e22736562734a4
Nikolas Howard <nikolas.howard@LighthouseSystems.com>
2019-05-20 17:25:32 +0100

    Added typings file

---
+++ dist/mistreevous.d.ts
@@ -0,0 +1,19 @@
+// Type definitions for Mistreevous 0.0.1
+// Project: Mistreevous
+// Definitions by: nikolas howard <https://github.com/nikkorn>
+declare module "Mistreevous" {
+	enum State {
+		READY,
+		RUNNING,
+		SUCCEEDED,
+		FAILED
+	}
+
+	declare class BehaviourTree {
+		constructor(definition: string, board: any);
+		step(): void;
+		reset(): void;
+		isRunning(): boolean;
+		getState(): State;
+	}
+}
