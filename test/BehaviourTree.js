9610d3a91c4abcd53629f0b2cdd2f7a1d9fd0ad6
nikkorn <nikhoward1234@hotmail.co.uk>
2020-05-29 17:48:31 +0100

    Updating README and fixed typings for 2.0.0

--- test/BehaviourTree.js
+++ test/BehaviourTree.js
@@ -1,5 +1,4 @@
-const Mistreevous = require('../dist/index');
-
+const mistreevous = require('../dist/index');
 const chai = require('chai');
 
 var assert = chai.assert;
@@ -8,11 +7,11 @@ describe("A BehaviourTree instance", () => {
   describe("has initialisation logic that", () => {
     describe("should error when", () => {
       it("the tree definition argument is not a string", () => {
-        assert.throws(() => new Mistreevous.BehaviourTree(null, {}), Error, "the tree definition must be a string");
+        assert.throws(() => new mistreevous.BehaviourTree(null, {}), Error, "the tree definition must be a string");
       });
 
       it("the blackboard object is not defined", () => {
-        assert.throws(() => new Mistreevous.BehaviourTree("", undefined), Error, "the blackboard must be defined");
+        assert.throws(() => new mistreevous.BehaviourTree("", undefined), Error, "the blackboard must be defined");
       });
     });
   });
