8be9915d714e667dfec28561d30b69a5d4106db1
Zenobius <airtonix@users.noreply.github.com>
2020-07-12 20:07:15 +0930

    also use apply here

--- src/nodes/condition.js
+++ src/nodes/condition.js
@@ -5,11 +5,11 @@ import State from "../state";
  * A Condition leaf node.
  * This will succeed or fail immediately based on a board predicate, without moving to the 'RUNNING' state.
  * @param decorators The node decorators.
- * @param condition The name of the condition function. 
+ * @param condition The name of the condition function.
  */
 export default function Condition(decorators, condition, conditionArguments) {
     Leaf.call(this, "condition", decorators);
-   
+
     /**
      * Update the node.
      * @param board The board.
@@ -18,7 +18,11 @@ export default function Condition(decorators, condition, conditionArguments) {
     this.onUpdate = function(board) {
         // Call the condition function to determine the state of this node, but it must exist in the blackboard.
         if (typeof board[condition] === "function") {
-            this.setState(!!(board[condition].call(board, conditionArguments || [])) ? State.SUCCEEDED : State.FAILED);
+            this.setState(
+                !!(board[condition].apply(board, conditionArguments || []))
+                    ? State.SUCCEEDED
+                    : State.FAILED
+            );
         } else {
             throw `cannot update condition node as function '${condition}' is not defined in the blackboard`;
         }
