2ef7b4d2705cb8c36ca197fe9ddd157b427094bf
Nikolas Howard <nikolas.howard@infor.com>
2022-10-07 08:24:08 +0100

    Added live reload for rollup

---
+++ rollup.config.js
@@ -0,0 +1,10 @@
+export default [
+    {
+      input: "./src/index.js",
+      output: {
+        file: "./dist/index.js",
+        format: "umd",
+        name: "mistreevous"
+      }
+    }
+  ];
